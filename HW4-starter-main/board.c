c0c18879e053cac1b5e2561c7ff63ed166086d5e
two2012 <guowen1124@yahoo.com>
2023-05-10 12:46:41 -0700

    initail completed

--- HW4-starter-main/board.c
+++ HW4-starter-main/board.c
@@ -1,13 +1,12 @@
 /**
  * Assignment: life
- * Name :TODO
- * PID: TODO
+ * Name :Wen Guo
+ * PID: A17630856
  * Class: UCSD CSE30-SP21
  *
  */
 #include "cse30life.h"
 #include "board.h"
-
 
 /**
  * create a new board
@@ -18,7 +17,7 @@
  * - read the first line which is the number of rows
  * - read the second line which is the number of cols
  * - set the # of rows and # of cols in the boards structure
- * - malloc bufferA and bufferB 
+ * - malloc bufferA and bufferB
  * - Set currentBuffer and nextBuffer
  * - clear both board buffers
  * - read the file until done.  each row contains a row and a columns separted by
@@ -27,38 +26,103 @@
  * - close the file
  * - return the boards pointer if successfull or NULL ptr otherwise
  */
-boards_t * createBoard(char *initFileName){
-  // TODO: 
+boards_t *createBoard(char *initFileName)
+{
+  // TODO:
+  FILE *fp;
+  // malloc a boards structure
+  boards_t *board = malloc(sizeof(boards_t));
+  // set the generation to 0
+  board->gen = 0;
+  // open file
+  if ((fp = fopen(initFileName, "r")) == NULL)
+  {
+    fclose(fp);
+    free(board);
+    return NULL;
+  }
+  // read the first two lines which are the number of rows and cols
+  if (fscanf(fp, "%zd", &board->numRows) != 1)
+  {
+    fclose(fp);
+    free(board);
+    return NULL;
+  }
+  if (fscanf(fp, "%zd", &board->numCols) != 1)
+  {
+    fclose(fp);
+    free(board);
+    return NULL;
+  }
+  // malloc bufferA and bufferB
+  board->bufferA = calloc(board->numRows * board->numCols, sizeof(belem));
+  board->bufferB = calloc(board->numRows * board->numCols, sizeof(belem));
+  if (!board->bufferA || !board->bufferB)
+  {
+    fclose(fp);
+    free(board->bufferA);
+    free(board->bufferB);
+    free(board);
+    return NULL;
+  }
+  // set currentBuffer and nextBuffer
+  board->currentBuffer = board->bufferA;
+  board->nextBuffer = board->bufferB;
+  // clear both board buffers
+  clearBoards(board);
+  // read the file until done and set the cell in the current buffer
+  int row, col;
+  while (fscanf(fp, "%d %d", &row, &col) == 2)
+  {
+    board->currentBuffer[(row * board->numCols) + col] = 1;
+    board->nextBuffer[(row * board->numCols) + col] = 1;
+  }
+
+  fclose(fp);
+  return board;
 }
-
-
 
 /**
  * delete a board
  */
-void deleteBoard(boards_t **bptrPtr){
+void deleteBoard(boards_t **bptrPtr)
+{
   // TODO:
+  // free the memory associated with the board
+  free((*bptrPtr)->bufferA);
+  free((*bptrPtr)->bufferB);
+  free(*bptrPtr);
+  *bptrPtr = NULL;
 }
 
 /**
  * set all the belems in both buffers to 0
  */
-void clearBoards(boards_t *self){
+void clearBoards(boards_t *self)
+{
   // TODO:
+  // set all values in bufferA and bufferB to 0
+  memset(self->bufferA, 0, self->numRows * self->numCols * sizeof(belem));
+  memset(self->bufferB, 0, self->numRows * self->numCols * sizeof(belem));
 }
 
 /**
  * swap the current and next buffers
  */
-void swapBuffers(boards_t *self){
+void swapBuffers(boards_t *self)
+{
   // TODO:
+  // swap the current and next buffers
+  belem *temp = self->currentBuffer;
+  self->currentBuffer = self->nextBuffer;
+  self->nextBuffer = temp;
 }
-
 
 /**
  * get a cell index
  */
-size_t getIndex(size_t numCols, size_t row, size_t col){
+size_t getIndex(size_t numCols, size_t row, size_t col)
+{
   // TODO:
+  return row * numCols + col;
 }
-  
--- HW4-starter-main/sim.c
+++ HW4-starter-main/sim.c
@@ -1,7 +1,7 @@
 /**
  * Assignment: life
  * Name: TODO
- * PID: TODO
+ * PID: A17630856
  * Class: UCSD CSE30-SP21
  *
  */
@@ -13,19 +13,65 @@ extern void asm_doRow(belem *, belem *, size_t, size_t, size_t);
 /**
  * gets x mod N (works for negative numbers as well! Use this instead of %)
  */
-size_t getModVal(int x, size_t N){
-     size_t adj = x/N;
+size_t getModVal(int x, size_t N)
+{
+  size_t adj = x / N;
 
-     return((x+adj*N)%N);
+  return ((x + adj * N) % N);
 }
 
 /**
  * process one row of the board
  */
-static void doRow(belem *dest, belem *src, size_t row, size_t rows, size_t cols){
-  // TODO: 
-}
+static void doRow(belem *dest, belem *src, size_t row, size_t rows, size_t cols)
+{
+  // TODO:
+  // calculate the alive neighbors for each cell in the row
+  for (size_t i = 0; i < cols; i++)
+  {
+    int alive_neighbors = 0;
 
+    // check surrounding cells for each cell in the row
+    for (size_t j = -1; j < 2; j++)
+    {
+      for (size_t k = -1; k < 2; k++)
+      {
+        if (j == 0 && k == 0)
+        {
+          continue;
+        }
+        if (src[(getModVal(row + j, rows) * cols) + k] == 1)
+        {
+          alive_neighbors++;
+        }
+      }
+    }
+
+    // update the cell state in the destination buffer
+    if (src[row * cols + i] == 1)
+    { // If the cell is alive
+      if (alive_neighbors < 2 || alive_neighbors > 3)
+      {
+        dest[row * cols + i] = 0; // The cell dies
+      }
+      else
+      {
+        dest[row * cols + i] = 1; // The cell stays alive
+      }
+    }
+    else
+    { // If the cell is dead
+      if (alive_neighbors == 3)
+      {
+        dest[row * cols + i] = 1; // The cell becomes alive
+      }
+      else
+      {
+        dest[row * cols + i] = 0; // The cell stays dead
+      }
+    }
+  }
+}
 
 /**
  * perform a simulation for "steps" generations
@@ -34,6 +80,18 @@ static void doRow(belem *dest, belem *src, size_t row, size_t rows, size_t cols)
  *   calculate the next board
  *   swap current and next
  */
-void simLoop(boards_t *self, unsigned int steps){
+void simLoop(boards_t *self, unsigned int steps)
+{
   // TODO:
+
+  for (size_t i = 0; i < steps; i++)
+  {
+    // calculate the next board
+    for (size_t j = 0; j < self->numRows; j++)
+    {
+      doRow(self->nextBuffer, self->currentBuffer, j, self->numRows, self->numCols);
+    }
+  }
+  // swap current and next
+  swapBuffers(self);
 }
