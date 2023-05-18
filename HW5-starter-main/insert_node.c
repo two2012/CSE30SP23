48de4f3845b6518a77625e89505a173ae156a6ba
two2012 <58891815+two2012@users.noreply.github.com>
2023-05-17 18:22:13 -0700

    update

--- HW5-starter-main/insert_node.c
+++ HW5-starter-main/insert_node.c
@@ -3,10 +3,6 @@
 #include "node.h"
 
 node *insert_node(node *front, char* id, double x, double y) {
-	(void) front; // suppress unused variable warnings
-	(void) id; // TODO: delete these once you start implementing insert_node
-	(void) x;
-	(void) y;
 	
 	// TODO: create a node, copy in the parameter fields
 	// make sure that you do a deep copy
@@ -28,7 +24,16 @@ node *insert_node(node *front, char* id, double x, double y) {
 	new_node->ycoord = y;
 
 	// TODO: link the new node into the chain 
-	new_node->next = front;
+	if (front == NULL)
+	{
+		front = new_node;
+		
+	}
+	else
+	{
+		new_node->next = front;
+	}
+	
 	// TODO: return the new head of the chain if the function succeeded
 	
 	return new_node; // TODO: replace this once you implement
